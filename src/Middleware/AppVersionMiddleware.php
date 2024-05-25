fec036bcb9a04451aa355e6fbccc611f168df8fd
Daniel Newman <dan@flytedesk.com>
2024-05-25 19:39:30 -0300

    Adding App Version mismatch middleware

---
+++ src/Middleware/AppVersionMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Newms87\Danx\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AppVersionMiddleware
+{
+	public function handle(Request $request, Closure $next)
+	{
+		$response = $next($request);
+
+		if (config('app.version')) {
+			$response->header('X-App-Version', config('app.version'));
+
+			// Append to Access-Control-Expose-Headers to expose the custom header
+			$exposeHeaders = $response->headers->get('Access-Control-Expose-Headers');
+			if ($exposeHeaders) {
+				$response->header('Access-Control-Expose-Headers', $exposeHeaders . ', X-App-Version');
+			} else {
+				$response->header('Access-Control-Expose-Headers', 'X-App-Version');
+			}
+		}
+
+		return $response;
+	}
+}
