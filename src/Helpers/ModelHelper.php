f18fcf27517604396fbd4440ece07da6fbbbf21a
Daniel Newman <dan@flytedesk.com>
2024-08-18 19:33:58 -0600

    ModelHelper::getNextModelName()

--- src/Helpers/ModelHelper.php
+++ src/Helpers/ModelHelper.php
@@ -3,12 +3,30 @@
 namespace Newms87\Danx\Helpers;
 
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use ReflectionClass;
 use Symfony\Component\Finder\Finder;
 
 class ModelHelper
 {
+	/**
+	 * Searches the database for the next available model name
+	 */
+	public static function getNextModelName(Model $model, $fieldName = 'name')
+	{
+		$count    = 0;
+		$baseName = trim(preg_replace("/\\(\\d+\\)$/", '', trim($model->{$fieldName})));
+
+		do {
+			$count++;
+			$newName = "$baseName ($count)";
+			dump("checked", $model::query()->where($fieldName, $newName)->toSql(), $model::query()->where($fieldName, $newName)->getBindings());
+		} while($model::query()->where($fieldName, $newName)->exists());
+
+		return $newName;
+	}
+
 	public static function getModelsWithTrait($trait)
 	{
 		$models    = [];
